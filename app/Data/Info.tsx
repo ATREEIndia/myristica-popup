febcb8ad6c6039746160820918a7811f9fd7555c
Sreekuttan V N <sreekuttan@atree.org>
2026-09-03 21:48:05 +0530

    first commit

---
+++ app/Data/Info.tsx
@@ -0,0 +1,205 @@
+export const info=[
+    {
+        
+        position:{left:'65.6%',top:'22.4%'},
+        img_url:"/hornbill.jpg",
+        title:"Malabar Grey Hornbill",
+        sciName:"Ocyceros griseus",
+        content:<span>
+            India is home to nine species of hornbills, of which four are found in the Western Ghats. These large, predominantly frugivorous birds mate for life: partnerships are formed after competing males perform mid-air acrobatics for potential mates. The winning male presents fruits to his female counterpart, and the pair then go off in search of a suitable tree cavity in which to lay their nest. The female hornbill enters the cavity alone, plastering and sealing herself inside with only a small slit left open for contact with her mate. Through this opening, the male hornbill slips his young family a variety of insects and fruits, including figs and <i>Myristica</i>.
+
+        </span>
+    },
+    {
+       
+        position:{left:'95.6%',top:'32.4%'},
+        img_url:"/02-04-tree-fern.jpg",
+        title:"Nilgiri Tree Fern",
+        sciName:"Cyathea nilgiriensis",
+        content:<span>
+           Ferns are a fascinating group of non-flowering plants that reproduce through spores rather than fruits, flowers and seeds. They originated during the Carboniferous period (358 mya), and many are still in existence—like this south Indian endemic tree fern that probably evolved in the early Cretaceous. About 250 mya ferns dominated Earth’s vegetation, but were slowly overtaken by seed-bearing, flowering plants. Botanists study them to understand the evolution of plant vascular systems.
+
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'47.6%',top:'30.4%'},
+        img_url:"/02-04-macaque-768x579.jpg",
+        title:"Lion-Tailed Macaques",
+        sciName:"Macaca silenus",
+        content:<span>
+          LTMs are Old World monkeys related to the first macaques that arrived from Africa about 5 mya, but now endemic to the Western Ghats. These shy ‘specialist’ primates thrive in select habitats with familiar food sources, such as Myristica swamps. Roaming in groups of 10–25 and up to 60, they feast on fruit like jackfruit and wild durian, while also serving as important seed dispersers for the swamp species Semecarpus <i>kathalekanensis</i>.
+
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'33.6%',top:'41.4%'},
+        img_url:"/02-04-squirrel-768x579.jpg",
+        title:"Malabar Giant Squirrel",
+        sciName:"Ratufa indica",
+        content:<span>
+          One of the largest squirrels in the world, they are typically solitary animals that only come together for breeding. They are believed to play a substantial role in shaping the ecosystem of their habitat by engaging in seed dispersal, though their diet also includes flowers and tree bark.
+
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'27.6%',top:'47.4%'},
+        img_url:"/02-04-lizard-768x579.jpg",
+        title:"Indian Flying Lizard",
+        sciName:"Draco dussumieri",
+        content:<span>
+          A brown, patchy body helps dracos camouflage perfectly against coarse, woody tree trunks. But their full glory is revealed when they spread their wing-like extensions (patagia), allowing them to glide gracefully between trees. Otherwise kept folded on either side of their bodies, the patagium acts like a parachute, helping dracos soar over long distances. Additionally, they possess a unique ‘dewlap’ or bright yellow-green extension from under the chin, which they wiggle up and down as a form of communication and display.
+
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'13.6%',top:'57.4%'},
+        img_url:"/02-04-lophopetalum-768x579.jpg",
+        title:"Wight’s Crest-Petal",
+        sciName:"Lophopetalum wightianum",
+        content:<span>
+          Often, <i>Myristica</i> swamps are found alongside evergreen forests. Evergreen species like <i>Lophopetalum wightianum</i> , Elaeocarpus tuberculatus, Dipterocarpus indicus are found mostly in such sacred forests, which have been protected for centuries by local communities. A study found that the basal area of trees (i.e. the thickness of their trunks) in <i>Myristica</i> swamps is much higher than elsewhere, indicating that these swamps are mature, old growth habitats, practically impossible to replace. This gigantic <i>Lophopetalum</i> tree also has large, snaking ‘serpentine’ roots, which give it support in swampy soils.
+
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'88.6%',top:'60.4%'},
+        img_url:"/02-04-mouse-deer-768x579.jpg",
+        title:"Indian Mouse Deer (Chevrotain)",
+        sciName:"Moschiola indica",
+        content:<span>
+       These nocturnal ungulates are only 54 cm in length on average—the smallest hoofed mammals in the world!
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'78.6%',top:'65.4%'},
+        img_url:"/02-04-myristica-768x579.jpg",
+        title:"Magnificent Nutmeg",
+        sciName:"Myristica magnifica",
+        content:<span>
+      Perhaps the most distinctive member of the family, with stilt roots rising from the base of the tree stem. A close relative of ‘true nutmeg’ (<i>Myristica fragrans,</i> native to the Spice Islands of Indonesia), evidenced by their similar fruit and seeds. We derive the valuable nutmeg spice from the seed of this Indonesian fruit, while its brightly coloured seed coating or ‘aril’ gives us the mace spice.
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'60.6%',top:'66.4%'},
+        img_url:"/02-04-warblers-768x579.jpg",
+        title:"Indian Scimitar Babbler",
+        sciName:"Pomatorhinus horsfieldii",
+        content:<span>
+      These birds feed on insects in the understorey of the forest. Hopping on the ground, they may sometimes turn over leaves or poke at dry leaves in search of prey.
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'35.6%',top:'66.4%'},
+        img_url:"/02-04-semecarpus-768x579.jpg",
+        title:"Kathlekan Marsh Nut (tree sapling)",
+        sciName:"Semecarpus kathalekanensis",
+        content:<span>
+     In the year 2000, scientists discovered a new tree species with large leaves and spongy knee roots arising from the swampy soils. It was named after the Kathlekan swamps of Uttara Kannada, which translates to ‘dark forests’. Such a recent discovery indicates that there is still so much to be discovered from these forests!
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'44.6%',top:'72.4%'},
+        img_url:"/02-04-damselfly-768x579.jpg",
+        title:"Myristica Bambootail",
+        sciName:"Phylloneura westermanni",
+        content:<span>
+    Besides their ancient pedigree, damselflies are known for elaborate courtship rituals, designed to show off the males’ bright colours or flying abilities. Once a mate has been chosen, the pair cling together in a distinctive posture called a ‘mating wheel’ or ‘heart’.
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'54.6%',top:'79.4%'},
+        img_url:"/02-04-mercuriana-768x579.jpg",
+        title:"Myristica Swamp Tree Frog",
+        sciName:"Mercurana myristicapalustris",
+        content:<span>
+   In 2013, researchers <b>Robin Abraham</b> and <b>Dr Anil Zachariah</b> discovered the Myristica Swamp Tree Frog, (so far only found in this habitat). The rusty brown males (only about 3.5cm long) gather at breeding sites, perching atop understory vegetation, like Pandan. From there, they call out to potential female mates, who are slightly larger, with bright green and yellow bodies. Individual male calls aren’t loud but occasionally they become synchronised, resulting in a chorus which can be heard about 20m away! The researchers were so surprised by this tuneful chorus that they named the frog after Freddy Mercury, the lead singer of rock band Queen. Male frogs were even observed engaging in physical combat with each other—wrestling, kicking and screeching to protect their turf. Once a female chooses her mate, they descend to the forest floor where she lays her eggs.
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'73.6%',top:'76.4%'},
+        img_url:"/02-04-pit-viper-768x579.jpg",
+        title:"Malabar Pit Viper",
+        sciName:"Trimersurus malabaricus",
+        content:<span>
+  Other snakes in this habitat include the Python (<i>Python molurus</i>), King Cobra (<i>Ophiophagus hannah</i>) and Humpnosed Pit Viper (<i>Hypnale hypnale</i>).
+        </span>
+    }
+    ,
+    {
+       
+        position:{left:'15.6%',top:'72.4%'},
+        img_url:"/02-04-gymnacranthera-768x579.jpg",
+        title:"Kanara Nutmeg",
+        sciName:"Gymnacranthera canarica",
+        content:<span>
+  Another distinctive, endemic tree, its ‘knee roots’ loop out of the soil surrounding the tree trunk, and have large air gaps called lenticels. Since these plants respire anaerobically, there is a high build-up of toxins such as acetaldehyde, ethyl alcohol and ethylene. Lenticels allow swamp plants to get rid of toxic waste, while also helping the plant to breathe in fresh air in oxygen-starved soils. A study on <i>Myristica</i> swamps of Travancore found that knee roots also oxidate soils around them.
+        </span> 
+    }
+    ,
+    {
+       
+        position:{left:'10.6%',top:'74.4%'},
+        img_url:"/02-04-crab.jpg",
+        title:"Crab",
+        sciName:"Barytelphusa guerini",
+        content:<span>
+ In 2013, PhD student <b>Shivani Krishna</b> observed these small crabs emerging at night to carry Myristica seeds into their burrows, sometimes as far as two metres. Interestingly, while the crabs consumed the tasty arils, they didn’t eat the seeds. Instead, their burrows appeared to function as a safe refuge for seeds, from which they could escape predation by other animals and germinate when the time was right!
+        </span> 
+    }
+    ,
+    {
+       
+        position:{left:'20.6%',top:'79.4%'},
+        img_url:"/02-04-arenga.jpg",
+        title:"Wight’s Sago Palm",
+        sciName:"Arenga wightii",
+        content:<span>
+An endemic Indian palm. Spot two other palm species in the swamp.
+        </span> 
+    }
+    ,
+    {
+       
+        position:{left:'35.6%',top:'85.4%'},
+        img_url:"/02-04-micrixalus-768x579.jpg",
+        title:"Kottigehar Dancing Frog",
+        sciName:"Micrixalus kottigeharensis",
+        content:<span>
+The frog family Micrixalidae diverged from all other amphibian species about 70 mya—around 5 my before the extinction of dinosaurs—making these little creatures very unique. Being under 3 cm long, the brown male is fairly inconspicuous in swampy leaf litter. With the din of rushing water in the swamp, its modest call isn’t enough to attract a mate. Instead, it dances, kicking back its hind legs and flashing its toe webs. Scientists call this ‘foot-flagging’, hypothesising that male frogs use this movement both to lure females and to fight off rival males!
+        </span> 
+    }
+    ,
+    {
+       
+        position:{left:'40.6%',top:'93.4%'},
+        img_url:"/02-04-gollum.jpg",
+        title:"Gollum Snakehead",
+        sciName:"Aenigmachanna gollum",
+        content:<span>
+Unlike many other fish, snakeheads can thrive in deep, low-oxygen waters thanks to a special breathing organ that allows them to subsist on swallowed air. Scientists believe that Gollum Snakeheads in particular evolved at least 109 mya, when the Indian plate had broken away from Gondwanaland and begun its northward drift. Quite probably, isolation brought on by continental breakup gave rise to this unique, ancient fish. Living in the depths of subterranean aquifers, Gollum Snakeheads seem to have survived the very same volcanic activity that spelled doom for the dinosaurs!
+        </span> 
+    }
+
+]
